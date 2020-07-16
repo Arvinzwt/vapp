0faadfd3630cff78504a1bbcd7147192b80e359d
Arvinzwt <zhangwentao577614@gmail.com>
2020-07-16 16:51:22 +0800

    update

--- layouts/blank.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <el-container class="jr">
-        <el-container class="jr-container">
-            <nuxt class="jr-main-blankness"/>
-        </el-container>
-    </el-container>
-</template>
