17b9ae33c65b0f20a12341c3a9eea600c0049998
Arvinzwt <zhangwentao577614@gmail.com>
2020-09-01 11:34:08 +0800

    添加富文本插件

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,45 @@
 <template>
-    <el-main></el-main>
+    <el-main class="wmm-demo">
+        <div id="fId"></div>
+    </el-main>
 </template>
 
 <script>
-export default {}
+
+export default {
+    head: {
+        script: [
+            {src: '/libs/ckeditor4/ckeditor.js'},
+            {src: '/libs/ckeditor4/samples/js/sample.js'},
+            {src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'},
+        ]
+    },
+    data() {
+        return {
+            textarea: false
+        }
+    },
+    mounted() {
+        this.textarea = CKEDITOR.replace('fId');//保存富文本操作对象
+        this.getTextarea();
+        this.textarea.on('change', function (event) {
+            localStorage.setItem('textarea', this.getData())
+        });
+    },
+    methods: {
+        getTextarea() {
+            let textarea = localStorage.getItem('textarea');
+
+            if (textarea) {
+                this.textarea.setData(textarea)
+            }
+        },
+    }
+}
 </script>
 
-<style>
-
+<style lang="scss">
+.wmm-demo {
+    padding-top: 0;
+}
 </style>
