7bf4aa4222e877ba854fd54615a645d349f1e274
Arvinzwt <zhangwentao577614@gmail.com>
2020-10-22 18:28:39 +0800

    菜单管理

--- api/module/common.js
+++ api/module/common.js
@@ -21,4 +21,148 @@ export default class Common extends Request {
             name: "张三"
         }
     }
+
+    // 获取菜单
+    async getMenu(data) {
+        return [
+            {
+                "name": "customer",//唯一属性 - 如果是页面取页面name属性，如果不是取对应文件夹名
+                "code": "1098",//权限码
+                "show": true,//是否在左侧菜单栏显示显示 show===true+拥有权限 才会显示
+                "title": "线索客户管理",//显示内容
+                "icon": "iconxueyuan",//显示图标
+                "child": [
+                    {
+                        "name": "customer-customer-manage",//唯一属性， 如果是页面取页面name属性，如果不是取对应文件夹名
+                        "code": "0988",//权限码
+                        "show": true,//是否在左侧菜单栏显示显示
+                        "title": "线索客户管理",//显示内容
+                        "num": 0,//拥有消息数量
+                        "path": "/customer/customer-manage",//页面路由
+                        "query": {},//页面参数，默认空
+                        'child': [//子路由
+                            {
+                                "name": "customer-customer-manage",//唯一id，页面name属性
+                                "code": "0988",//权限码
+                                "show": true,//是否在左侧菜单栏显示显示
+                                "title": "线索客户管理-新海",
+                                "num": 0,//小时数量
+                                "path": "/customer/customer-manage",//子页面路由
+                                "query": {}//子页面参数
+                            },
+                            {
+                                "name": "customer-customer-manage-second",
+                                "code": "0988",
+                                "show": false,
+                                "title": "线索客户管理-公海",
+                                "num": 0,
+                                "path": "/customer/customer-manage/second",
+                                "query": {}
+                            },
+                            {
+                                "name": "customer-customer-manage-third",
+                                "code": "0988",
+                                "show": false,
+                                "title": "线索客户管理-死海",
+                                "num": 0,
+                                "path": "/customer/customer-manage/third",
+                                "query": {}
+                            },
+                            {
+                                "name": "customer-customer-manage-fourth",
+                                "code": "0988",
+                                "show": false,
+                                "title": "线索客户管理-全部",
+                                "num": 0,
+                                "path": "/customer/customer-manage/fourth",
+                                "query": {}
+                            },
+                        ]
+                    },
+                    {
+                        "name": "customer-customer-call",
+                        "code": "0987",
+                        "show": true,
+                        "title": "呼入线索客户",
+                        "num": 0,
+                        "path": "/customer/customer-call",
+                        "query": {},
+                    },
+                    {
+                        "name": "customer-today-clue",
+                        "code": "0989",
+                        "show": true,
+                        "title": "今日新线索",
+                        "num": 6,
+                        "path": "/customer/today-clue",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-today-reserve",
+                        "code": "0990",
+                        "show": true,
+                        "title": "今日预约沟通",
+                        "num": 0,
+                        "path": "/customer/today-reserve",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-customer-recover",
+                        "code": "0991",
+                        "show": true,
+                        "title": "即将回收",
+                        "num": 0,
+                        "path": "/customer/customer-recover",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-my-customer",
+                        "code": "0992",
+                        "show": true,
+                        "title": "我的线索客户",
+                        "num": 0,
+                        "path": "/customer/my-customer",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-my-student",
+                        "code": "0994",
+                        "show": true,
+                        "title": "我的学员",
+                        "num": 0,
+                        "path": "/customer/my-student",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-nov-record",
+                        "code": "0993",
+                        "show": true,
+                        "title": "诺访记录",
+                        "num": 0,
+                        "path": "/customer/nov-record",
+                        "query": {}
+                    },
+
+                    {
+                        "name": "customer-recommend-check",
+                        "code": "0995",
+                        "show": true,
+                        "title": "推荐审核",
+                        "num": 0,
+                        "path": "/customer/recommend-check",
+                        "query": {}
+                    },
+                    {
+                        "name": "customer-customer-detail",
+                        "code": "0998",
+                        "show": false,
+                        "title": "用户详情",
+                        "num": 0,
+                        "path": "/customer/customer-detail",
+                        "query": {}
+                    },
+                ]
+            },
+        ]
+    }
 }
--- components/Nav.vue
+++ components/Nav.vue
@@ -4,7 +4,10 @@
         <el-tag size="small" class="jr-nav_tag"
                 v-for="item in topMenu"
                 :key="item.name"
-                :closable="topMenu.length>1">
+                :type="item.name===$route.name?'':'info'"
+                :closable="topMenu.length>1"
+                @click="tagClick(item)"
+                @close="tagClose(item)">
             {{ item.title }}
         </el-tag>
     </div>
@@ -14,14 +17,116 @@
 export default {
     data() {
         return {
-            topMenu: [//顶部菜单信息
-                {
-                    name:2123,
-                    title:'页面'
-                }
-            ],
+            topMenu: [],//顶部菜单信息
+            asideMenu: [],//菜单信息
         }
     },
+    watch: {
+        // 监听路由变化并存储下来
+        async "$route"(to, form) {
+            let tm = this.getLocalStorageTm();//local存储的路由信息
+            let target = tm.find(item => {//找到当前操作的页面在历史存储中的对象
+                return item.name === to.name;
+            })
+
+            if (!target) {//如果没有该路由，加入
+                tm.push({
+                    name: to.name,
+                    query: to.query,
+                });
+                localStorage.setItem('tm', JSON.stringify(tm));//本地存储
+            } else {//更新query信息
+                target.query = to.query;
+                localStorage.setItem('tm', JSON.stringify(tm));//本地存储
+            }
+
+            this.resetTopMenu();//重新刷新topMenu数据
+        }
+    },
+    mounted() {
+        this.$api.common.getMenu().then(menu => {//拉取菜单信息
+            this.asideMenu = menu;
+            this.resetTopMenu();//重新刷新topMenu数据
+        });
+    },
+    methods: {
+        /**
+         *@desc 拉取本地存储的topMenu信息
+         */
+        getLocalStorageTm() {
+            return localStorage.getItem('tm') ? JSON.parse(localStorage.getItem('tm')) : [{
+                name: 'customer-customer-manage',
+                query: {}
+            }];
+        },
+
+        /**
+         *@desc 将本地存储的topMenu转换为数据对象
+         */
+        async resetTopMenu() {
+            let tm = this.getLocalStorageTm();//local存储的路由信息
+            let filTopMenu = (obj, target) => {
+                if (obj.name === target.name) {
+                    this.topMenu.push({
+                        ...obj,
+                        query: target.query
+                    })
+                }
+            }
+
+            this.topMenu = [];//清空topMenu
+            tm.forEach(tmItem => {//循环历史记录
+                this.asideMenu.forEach(mItem => {//循环菜单第一层
+                    mItem.child = mItem.child || [];
+                    mItem.child.forEach(mList => {//循环菜单第二层
+                        if (mList.child) {//如果有子路由
+                            mList.child.forEach(mLi => {//循环菜单第三层
+                                filTopMenu(mLi, tmItem)
+                            })
+                        } else {//如果没有子菜单
+                            filTopMenu(mList, tmItem)
+                        }
+                    })
+                })
+            })
+            return tm;
+        },
+
+        /**
+         *@desc topMenu点击
+         */
+        tagClick(obj) {
+            this.$router.push({
+                path: obj.path,
+                query: obj.query,
+            })
+        },
+
+        /**
+         *@desc topMenu关闭
+         */
+        tagClose(obj) {
+            if (this.topMenu.length > 1) {//保证至少有一个topmenu
+                let tm = this.getLocalStorageTm();//本地存储信息
+                tm.forEach((item, index) => {//找到操作对象
+                    if (item.name === obj.name) {
+                        tm.splice(index, 1);//删除
+                    }
+                })
+                localStorage.setItem('tm', JSON.stringify(tm));//重置本地存储
+
+                this.resetTopMenu().then(res => {//重置topmenu数据
+                    if (obj.name === this.$route.name) {//如果删除的是当前路由
+                        let target = this.$utils.underscore.last(this.topMenu);//找到最后一个
+                        this.$router.push({//跳转
+                            path: target.path,
+                            query: target.query,
+                        })
+                    }
+                })
+            }
+        },
+    }
 }
 </script>
 
