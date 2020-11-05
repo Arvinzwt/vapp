290db13591bdf0f00756dfd2a3b763620f1e0ee3
Arvinzwt <zhangwentao577614@gmail.com>
2020-11-05 17:09:22 +0800

    跟进页面（半处理）

--- pages/customer/customer-follow.vue
+++ pages/customer/customer-follow.vue
@@ -1,23 +1,71 @@
 <template>
     <!--客户跟进-->
-    <el-main class="jr-customer-customer-follow"></el-main>
+    <el-main class="jr-customer-customer-follow">
+        <!--基本信息-->
+        <div class="bg-wrap">
+            <div class="jr-title">
+                <h3>基本信息</h3>
+                <div><el-button size="mini" type="primary">+标签</el-button></div>
+            </div>
+            <el-form class="jr-form" size="mini" :model="paramMap" label-width="90px" label-position="left">
+                <!--标签-->
+                <el-form-item label="标签">
+                    <el-tag size="small" type="primary">标签1</el-tag>
+                </el-form-item>
+                <el-row :gutter="15">
+                    <el-col :span="6">
+                        <el-form-item label="姓名">
+                            <el-input :maxlength='50' v-model="paramMap.str" placeholder="请输入内容" clearable/>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-form-item label="手机">
+                            <span class="mr-2">13122122212</span>
+                            <span class="mr-2 el-icon-phone-outline text-color-brand cursor-pointer"></span>
+                            <span class="el-icon-view text-color-brand cursor-pointer"></span>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </div>
+
+    </el-main>
 </template>
 
 <script>
 import PaginationTemplate from "@/components/customer/Pagination";
 import SelectedRoleTemplate from "@/components/customer/SelectedRole";
 
-export default {}
+export default {
+    components: {
+        PaginationTemplate,
+        SelectedRoleTemplate,
+    },
+    data() {
+        return {
+            // 筛选参数信息
+            paramMap: {
+                show: true,//是否显示筛选
+            },
+        }
+    }
+}
 </script>
 
 <style lang="scss">
 .jr-customer-customer-follow {
     min-width: 1000px;
 
+    .jr-title {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
     //块背景
     .bg-wrap {
         background-color: #fafafa; //常规
-        padding: 0 20px 5px;
+        padding: 5px 20px 5px;
         border-radius: 4px;
     }
 
